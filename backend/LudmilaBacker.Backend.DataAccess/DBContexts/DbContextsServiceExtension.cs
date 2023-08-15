0b05ebfcf55f838ac2f58f66957cef9c07318f06
NaMe2te <vladyushashishkin81@gmail.com>
2023-08-15 16:02:29 +0300

    feat: added DatabaseContext to service provider

---
+++ backend/LudmilaBacker.Backend.DataAccess/DBContexts/DbContextsServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LudmilaBacker.Backend.DataAccess.DBContexts;
+
+public static class DbContextsServiceExtension
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection collection,
+        Action<DbContextOptionsBuilder> options)
+    {
+        collection.AddDbContext<DatabaseContext>(options);
+        return collection;
+    }
+}
